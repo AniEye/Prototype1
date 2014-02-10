0802bd767f279a2eddc172c161627820eb10665d
jangerhard <jgschoepp@gmail.com>
2014-02-10 17:49:38 +0100

    - Updated JUnit test

--- ProtoType1/src/com/bbv/prototype1/JUnit/TilViskosTest.java
+++ ProtoType1/src/com/bbv/prototype1/JUnit/TilViskosTest.java
@@ -15,7 +15,23 @@ public class TilViskosTest extends AndroidTestCase {
 	}
 
 	public void testCalculationIntFloatFloat() {
-		//assertEquals("Testing calculation", "300.0", test.calculation(0, 56, 56));
+		
+		
+		//Uferdig kode for testing
+		
+//		//Testing TV
+//		assertEquals("300.0", test.calculation(0, 56, 56));
+//		assertEquals("150.0", test.calculation(0, 5, 10));
+//		
+//		//Testing Theta
+//		assertEquals("0.16666667", test.calculation(1, 10, 5));
+//		assertEquals("0.25", test.calculation(1, 15, 5));
+//		
+//		
+//		//Testing RPM
+//		assertEquals("150.0", test.calculation(0, 5, 10));
+//		assertEquals("300.0", test.calculation(0, 10, 10));
+		
 	}
 
 }
