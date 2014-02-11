e8452f312dce1ccc641c69d6c7b2aaf2dfbf5175
AniEye <laugaland_6@hotmail.com>
2014-02-11 14:17:53 +0100

    -la til en klasse

---
+++ ProtoType1/src/com/bbv/prototype1/Vis_Teori.java
@@ -0,0 +1,14 @@
+package com.bbv.prototype1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Vis_Teori extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.vis_teori);
+	}
+}
