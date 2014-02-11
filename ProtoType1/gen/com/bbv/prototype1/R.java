87d75f3e2738d60d62e1fc5513b677c7ecfdcce9
jangerhard <jgschoepp@gmail.com>
2014-02-11 15:41:33 +0100

    -Endret ikon

--- ProtoType1/gen/com/bbv/prototype1/R.java
+++ ProtoType1/gen/com/bbv/prototype1/R.java
@@ -43,7 +43,8 @@ public final class R {
         public static final int custom_background=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int logo_test=0x7f020003;
-        public static final int til_viskos_icon=0x7f020004;
+        public static final int logo_test2=0x7f020004;
+        public static final int til_viskos_icon=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f0a001c;
